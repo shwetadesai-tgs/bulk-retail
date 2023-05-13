4c0b3afce79f1fb41e543650b1c111d8db07871a
ChiragTriveni <131141764+ChiragTriveni@users.noreply.github.com>
2023-05-13 12:30:26 +0530

    add token identity server

---
+++ User/User.Core/Models/AuthToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User.Core.Models
+{
+    public class AuthToken
+    {
+        public string Access_token { get; set; }
+    }
+}
